cc761ff3777040b0b21b758eb70669cf6d31eabc
Yannick Biheul <yannickbiheul@outlook.fr>
2020-07-10 11:45:22 +0200

    Ajout des fichiers HTML et CSS et du dossier images

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# projet1
+# Projet1
 1er vrai projet : mon site.
+
+* **Étapes**
+    * Créer un site statique avec HTML et CSS.
+    * Rendre le site dynamique avec PHP et MySQL.
+    * Design responsive avec des Media Queries.
+    * Ajouter des animations avec JavaScript.
+
+* **Journal Juillet 2020**
+    * 10 Juillet : Ajout des fichiers HTML et CSS.
---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Yannick Biheul</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <script src="script.js" defer></script>
+</head>
+<body>
+
+    <!-- HEADER -->
+    <header>
+        <div class="headerTitle">
+            <a href="#accueil">Yannick Biheul</a>
+        </div>
+        <nav>
+            <a href="#apropos">À Propos</a>
+            <a href="#portfolio">Portfolio</a>
+            <a href="#contact">Contact</a>
+        </nav>
+    </header>
+
+    <!-- MAIN -->
+    <main>
+
+        <!-- ACCUEIL -->
+        <section id="accueil">
+            <div class="containerAccueil">
+                <h1>Reconversion Professionnelle</h1>
+                <p>Pilote de production > Développeur web</p>
+                <div class="button">
+                    <a href="#">Mon CV</a>
+                </div>
+            </div>
+        </section>
+
+        <!-- A PROPOS -->
+        <section id="apropos">
+            <div class="sectionTitle">
+                <h2>À Propos de moi</h2>
+                <div class="separation">
+                    <span></span>
+                </div>
+            </div>
+            <div class="containerApropos">
+                <div class="moi">
+                    <h3>Qui suis-je</h3>
+                    <p>Je m’appelle Yannick Biheul, j’ai 38 ans. Je suis né et je vis à Quimper, dans le Finistère.</p>
+                    <p>Je suis intéressé par les nouvelles technologies, les jeux vidéo, le cinéma, la musique et la lecture.</p>
+                    <p>Je travaille en tant que pilote de production chez un équipementier automobile, et souhaite devenir développeur web.</p>
+                </div>
+                <div class="scolaire">
+                    <h3>Parcours Scolaire</h3>
+                    <p>BEP ICEF (Installateur Conseil en Équipement du Foyer option Électroménager)
+                    en 1999.</p>
+                    <p>BAC PRO MAEMC (Maintenance des Appareils et Équipements Ménagers et de Collectivités)
+                    en 2001.</p>
+                    <p>BEP ATV (Agent Technique de Vente) en 2002.</p>
+                </div>
+                <div class="experience">
+                    <h3>Parcours Professionnel</h3>
+                    <p>Quelques missions intérim dans des usines agro-alimentaire entre 2001 et 2002.</p>
+                    <p>Vendeur chez Conforama à Quimper en 2002.</p>
+                    <p>Opérateur de production chez Livbag à Pont de Buis de 2003 à aujourd'hui.</p>
+                </div>
+            </div>
+        </section>
+
+        <!-- PORTFOLIO -->
+        <section id="portfolio">
+            <div class="sectionTitle">
+                <h2>Mon Portfolio</h2>
+                <div class="separation">
+                    <span></span>
+                </div>
+            </div>
+        </section>
+
+        <!-- CONTACT -->
+        <section id="contact">
+
+        </section>
+
+    </main>
+
+</body>
+</html>
